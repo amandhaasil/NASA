38d7b63a10d85b63f0a38872cca251780d896ba0
amandhaasil <amandhaasil@hotmail.com>
2017-01-18 21:20:20 -0200

    Funcionando o basico.

--- NASA/src/Main.java
+++ NASA/src/Main.java
@@ -1,4 +1,71 @@
+import java.util.List;
+import java.util.Scanner;
+
+import controller.MarsDroidController;
+import equipament.MarsDroid;
+
 
 public class Main {
 
+	private static int x, y;
+	private static char d;
+	
+	private static boolean processDroidPosition(String p){
+		if(p.charAt(0)=='q') return false;
+		
+		String[] position = p.split(" ");
+
+		x = Integer.parseInt(position[0]);
+		y = Integer.parseInt(position[1]);
+		d = position[2].charAt(0);
+		return true;
+	}
+	
+	public static void main(String args[]){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		MarsDroidController controller;
+		List<MarsDroid> droids;
+		
+		System.out.println("Welcome to NASA's program of Drois in Mars!");
+		
+		try{
+			System.out.println("Please enter integers X and Y that will mark"
+				+ " borders of the Cartesian Plane: ");
+			String[] borders = (scan.nextLine()).split(" ");
+		 
+			x = Integer.parseInt(borders[0]);
+			y = Integer.parseInt(borders[1]);
+			
+			controller = new MarsDroidController(x, y);
+			
+			String movements;
+			String newPosition = scan.nextLine();
+			while(processDroidPosition(newPosition)){
+				movements = scan.nextLine();
+				controller.addNewDroid(x, y, d, movements);
+				
+				newPosition = scan.nextLine();
+			}
+			
+			System.out.println("\nDroids final positions:");
+			droids = controller.getDroids();
+			MarsDroid aux;
+			for (int i = 0; i < droids.size(); i++){
+				aux = droids.get(i);
+				System.out.println(aux.getX() + " " + aux.getY() + " " + aux.getDirection());
+			}
+			
+			System.out.print("\n Thank you!");
+			
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+			System.err.println(e.getCause());
+		}finally{
+			scan.close();
+		}
+		
+		scan.close();
+	}
 }
--- NASA/src/controller/MarsDroidController.java
+++ NASA/src/controller/MarsDroidController.java
@@ -4,6 +4,7 @@
 package controller;
 import java.util.List;
 import java.util.LinkedList;
+
 import equipament.MarsDroid;
 
 /**
@@ -14,14 +15,60 @@ import equipament.MarsDroid;
  */
 public class MarsDroidController {
 	int xMax, yMax;
-	private List<MarsDroid> sondas = new LinkedList<MarsDroid>();
+	private List<MarsDroid> droids = new LinkedList<MarsDroid>();
 	
 	public MarsDroidController(int xMax, int yMax){
 		this.xMax = xMax; 
 		this.yMax = yMax;
 	}
 	
-	public void addNewDroid(){
+	
+	public void moveDroidN(int n, String m) throws Exception{
+		MarsDroid moving;
+		char aux;
 		
+		//check if droid exists
+		if(n>=droids.size()){
+			throw new Exception("This droid does not exist!");
+		}
+		moving = droids.get(n);
+		for(int i = 0; i < m.length(); i++){
+			aux = m.charAt(i);
+			
+			if(aux == 'R')
+				moving.turnRight();
+			else if(aux == 'L')
+				moving.turnLeft();
+			else if(aux == 'M'){
+				//se o droid nao estiver indo para fora dos limites pode se mover
+				if((moving.getDirection() == 'N' && moving.getY() != yMax)
+						|| (moving.getDirection() == 'E' && moving.getX() != xMax)
+						|| (moving.getDirection() == 'S' && moving.getY() != 0)
+						|| (moving.getDirection() == 'W' && moving.getX() != 0))
+					moving.move();
+				else throw new Exception("Droid attempted to leave the border."); 
+			}
+			else{
+				throw new Exception("Invalid command /'" + aux + "'/."
+						+ "/nPlease only use L, R or M with no space between.");
+			}
+		}
+	}
+	
+	public void addNewDroid(int x, int y, char d, String m)throws Exception{
+		//check if the position is valid
+		if(x<0||x>xMax||y<0||y>yMax){
+			throw new Exception("You can't create Droids out of the Borders."
+					+ "/nMust have 0 <= x <= " + xMax
+					+ " and 0 <= y <= " + yMax);
+			
+		}
+		MarsDroid newDroid = new MarsDroid(x, y, d);
+		droids.add(newDroid);
+		moveDroidN(droids.size() -1, m);		
+	}
+	
+	public List<MarsDroid> getDroids(){
+		return droids;
 	}
 }
